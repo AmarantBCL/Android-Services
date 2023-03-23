efa5888c3d4a1a58b4685e67f6f2ff0e3c413729
vpetel127 <vpetel127@cable.comcast.com>
2023-03-23 11:17:47 -0400

    Services

---
+++ app/src/main/java/com/amarant/apps/services_android/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.amarant.apps.services_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amarant.apps.services_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.btnService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
+    }
+}
