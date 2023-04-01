45270506e9d0251d7b474a79b0a21e357d0c27ee
vpetel127 <vpetel127@cable.comcast.com>
2023-04-01 10:04:10 -0400

    Restart Services

--- app/src/main/java/com/amarant/apps/services_android/MainActivity.kt
+++ app/src/main/java/com/amarant/apps/services_android/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.btnService.setOnClickListener {
-            startService(MyService.newIntent(this))
+            startService(MyService.newIntent(this, 25))
         }
     }
 }
