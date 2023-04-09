23eecc19a8315681b9a0547fdcdfa625ef7bcd24
vpetel127 <vpetel127@cable.comcast.com>
2023-04-09 07:46:14 -0400

    Job Scheduler

--- app/src/main/java/com/amarant/apps/services_android/MyJobService.kt
+++ app/src/main/java/com/amarant/apps/services_android/MyJobService.kt
@@ -1,11 +1,7 @@
 package com.amarant.apps.services_android
 
-import android.app.Service
 import android.app.job.JobParameters
 import android.app.job.JobService
-import android.content.Context
-import android.content.Intent
-import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -43,5 +39,9 @@ class MyJobService : JobService() {
 
     private fun log(message: String) {
         Log.d("SERVICE_TAG", "MyJobService: $message")
+    }
+
+    companion object {
+        const val JOB_ID = 111
     }
 }
